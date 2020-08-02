8bb10c35a8e034a54d3042b47a09262ae80abc75
Kirandeep Kaur <me@kiran.cc>
2020-08-02 12:41:47 +0530

    frontend charts complete

--- client/js/app.js
+++ client/js/app.js
@@ -1,16 +1,103 @@
-/*jslint browser:true */
-/*global
-alert, confirm, console, prompt
-*/
-/*global window */
-var ctx = document.getElementById('myChart').getContext('2d');
-console.log("kiran")
-var chart = new Chart(ctx, {
-  type: 'bar',
-  plugins: [ChartDataSource],
-  options: {
-    daatasource: {
-      url: 'sample-dataset.xlsx'
+const createChartForEntity = (duration, filePath) => {
+  var parseDate = d3.timeParse("%Y-%m-%d");
+  var margin = { left: 150, right: 20, top: 20, bottom: 50 };
+  var width = 860 - margin.left - margin.right;
+  var height = 400 - margin.top - margin.bottom;
+
+  var max = 0;
+
+  var xNudge = 50;
+  var yNudge = 20;
+
+  var minDate = new Date();
+  var maxDate = new Date();
+
+  d3.csv(filePath)
+    .row(function(d) {
+      return {
+        date: parseDate(d.Date),
+        price: Number(d.Price.trim().slice(1))
+      };
+    })
+    .get(function(error, rows) {
+      max = d3.max(rows, function(d) {
+        return d.price;
+      });
+      minDate = d3.min(rows, function(d) {
+        return d.date;
+      });
+      maxDate = d3.max(rows, function(d) {
+        return d.date;
+      });
+
+      var y = d3
+        .scaleLinear()
+        .domain([0, max])
+        .range([height, 0]);
+
+      var x = d3
+        .scaleTime()
+        .domain([minDate, maxDate])
+        .range([0, width]);
+
+      var yAxis = d3.axisLeft(y);
+
+      var xAxis = d3.axisBottom(x);
+
+      var line = d3
+        .line()
+        .x(function(d) {
+          return x(d.date);
+        })
+        .y(function(d) {
+          return y(d.price);
+        })
+        .curve(d3.curveCardinal);
+
+      var svg = d3
+        .select("body")
+        .append("svg")
+        .attr("id", "svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+      var chartGroup = svg
+        .append("g")
+        .attr("class", "chartGroup")
+        .attr("transform", "translate(" + xNudge + "," + yNudge + ")");
+
+      chartGroup
+        .append("path")
+        .attr("class", "line")
+        .attr("d", function(d) {
+          return line(rows);
+        });
+
+      chartGroup
+        .append("g")
+        .attr("class", "axis x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+      chartGroup
+        .append("g")
+        .attr("class", "axis y")
+        .call(yAxis);
+      chartGroup
+        .append("text")
+        .style("font-size", "20px")
+        .style("text-decoration", "underline")
+        .style("font-weight", "700")
+        .attr("x", width / 2)
+        .attr("y", 0 - margin.top / 322)
+        .text(duration + " Prices")
+        .attr("text-anchor", "right");
+    });
+};
+var map = {
+  Daily: "../data/csv/daily.csv",
+  Weekly: "../data/csv/weekly.csv",
+  Monthly: "../data/csv/monthly.csv",
+  Annual: "../data/csv/annual.csv"
+};
+    for(let dur in map) {
+    createChartForEntity(dur, map[dur]);
     }
-  }
-});
--- script/script.py
+++ script/script.py
@@ -5,22 +5,33 @@ import os
 import urllib
 
 
-HENRY_HUB_BASE_URL = 'https://www.eia.gov/dnav/ng/hist_xls/RNGWHHD{}.xls'
-_dir = "./data/xls"
+BASE_URL = 'https://www.eia.gov/dnav/ng/hist_xls/RNGWHHD{}.xls'
+DATE_COLUMN_XLS = 'Back to Contents'
+PRICE_COLUMN_XLS = 'Data 1: Henry Hub Natural Gas Spot Price (Dollars per Million Btu)'
+PRICE_COLUMN_WEEKLY_XLS = 'Data 1: Weekly Henry Hub Natural Gas Spot Price (Dollars per Million Btu)'
+
+_dir_xls = "../data/xls/"
+_dir_csv ="../data/csv/"
+
 def download_file(url,item):
 	if item=='d':
-		urllib.request.urlretrieve(url, 'gas-prices/data/xls/daily.xls')
+		urllib.request.urlretrieve(url, _dir_xls+'daily.xls')
 	elif item=='w':
-		urllib.request.urlretrieve(url, 'gas-prices/data/xls/weekly.xls')
+		urllib.request.urlretrieve(url, _dir_xls+'weekly.xls')
 	elif item=='m':
-		urllib.request.urlretrieve(url, 'gas-prices/data/xls/monthly.xls')
+		urllib.request.urlretrieve(url, _dir_xls+ 'monthly.xls')
 	else:
-		urllib.request.urlretrieve(url, 'gas-prices/data/xls/yearly.xls')
+		urllib.request.urlretrieve(url, _dir_xls +'annual.xls')
 
 def make_directory(path):
-	os.mkdir(path)
+	try:
+		os.mkdir(path)
+	except:
+		return False
+	return True
 
-	
+
+
 def get_urls(duration):
 	if duration=='d':
 		return 'https://www.eia.gov/dnav/ng/hist_xls/RNGWHHDd.xls'
@@ -31,19 +42,36 @@ def get_urls(duration):
 	else:
 		return 'https://www.eia.gov/dnav/ng/hist_xls/RNGWHHDa.xls'
 	
-def convert_to_csv(item):	
-	 data_xls = pd.read_excel('daily.xls', 'Data 1')
-	 data_xls.to_csv(item +'.csv')
+def convert_to_csv(item):
+	 data_xls = pd.read_excel(_dir_xls +item+'.xls', 'Data 1')
+	 data_xls['Date']= data_xls[DATE_COLUMN_XLS]
+	 if item=='weekly':
+		 data_xls['Price']= data_xls[PRICE_COLUMN_WEEKLY_XLS]
+	 else:
+		 data_xls['Price']= data_xls[PRICE_COLUMN_XLS]
+
+	 data_xls = data_xls[['Date', 'Price']].iloc[2:].set_index(['Date'])
+	 
+	 if item=='monthly':
+		 data_xls = normalize_df_date(data_xls)
+
+	 data_xls.to_csv(_dir_csv+item +'.csv')
+	 return data_xls
+
+def normalize_df_date(df):
+    df.index = df.index.map(lambda t: t.replace(day=1))
+    return df
 
 def main():	
+	make_directory(_dir_csv)
+	make_directory(_dir_xls)
 	map ={
 		'd':'daily',
 		'w':'weekly',
 		'm':'monthly',
 		'y':'annual'
 	}
-	list = ['d','w','m','y']
-	for item in list:
+	for item in map:
 		urls = get_urls(item)
 		download_file(urls, item)
 		convert_to_csv(map[item])	
